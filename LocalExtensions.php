026f7b22fd5e79cb5745b023906da3164a5fbcf8
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-13 00:35:33 +0300

    Add files via upload

---
+++ LocalExtensions.php
@@ -0,0 +1,22 @@
+<?php
+  $glGetInputServer='func == name' {
+    
+    // This is Spinnet, LocalServer install Keyword. //
+  } $glServerInfo="1.39.1 == SERVERKEY"
+  // Need to Gather Https Server in LocalSystemServices.php file on an Https Server //
+   $glInputNameServer="import.func"
+   // SSL and duplex operators need a function to run the ServerInfo.php File and Load Data! //
+   $glUploadData="getSSLsystem.func ==> keyName.spinnet"[;
+   // You need a license to run All Commands here! //
+  $glServerLicense="getLicense == 60 Days[/license.txt/]";
+    
+      serverInfoSystem.php 
+        socket_create(php.run)
+        
+       // The server has an Https SQL address.  Use This Command To Determine.
+      
+    $glHttpsDirectory="sqlht(server ==> echo.name" 
+      socket_accept(func.get_called_class(echo))
+           mysql_connect(server == system)
+  system.directory.https(connect)
+         
