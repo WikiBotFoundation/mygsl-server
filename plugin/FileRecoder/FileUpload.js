e7b05e3653f82d5b837f200654eef456a89f98fa
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-13 13:15:09 +0300

    Create FileUpload.js

---
+++ plugin/FileRecoder/FileUpload.js
@@ -0,0 +1,17 @@
+<upload>
+   <java>
+     <java>
+        FileUpload == coondition
+        shell(name == py
+         coordinate.file
+    <java>
+ <java>
+    <java>
+         FileUploader.(fileEditor.File
+          FileSystem.HttpsDirectory.php
+      <java>
+  <java>
+<java>
+       Https( == name import
+        CreateFile == (Upload.php
+      php.system
