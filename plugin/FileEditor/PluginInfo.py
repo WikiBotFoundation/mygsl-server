b0016b81feaca21c9df410160bd1edc7b996e811
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-13 13:30:32 +0300

    Create PluginInfo.py

---
+++ plugin/FileEditor/PluginInfo.py
@@ -0,0 +1,21 @@
+<index>
+  <index> plugin.gl(MyGSL == plugin)
+   ''''pluginversion="1.6.0"'''
+   "'""pluginname="FileEditor"'''
+   "'''tokenbulid="index.js"
+     "'''InfoDescription="File Editor System"'''
+    {
+     version.import
+      (java == edition
+              }
+        coondition.java 
+           UploadFile.editor
+        java == desc.__import__
+      plugin.edition 
+               plugin.desc(_ == editor
+       { java.plugin == desc
+              }
+         version.import
+                edition. == Desc.__
+
+   
