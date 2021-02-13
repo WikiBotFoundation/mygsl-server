224ebb4927259852dbd0265be788b35f6c6d5192
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-13 13:22:45 +0300

    Create index.js

---
+++ plugin/FileEditor/index.js
@@ -0,0 +1,22 @@
+<java>
+  <java>
+     File.moderator == mySQL
+      import.file == java
+     UploadFile. [ == Py
+     <java>
+<java>
+       File.java
+       Coordination,import
+          file.cpp(file
+  <java>
+<java>
+      click.fileUploadButton( == button.py
+      clicked.cpp( == Upload.file
+       System == /record/system/fileUpload
+<java>
+  <java> 
+    console.system(cpp)
+       file.upload(system/files/gl
+       file.system(files/upload/allfiles
+
+     
