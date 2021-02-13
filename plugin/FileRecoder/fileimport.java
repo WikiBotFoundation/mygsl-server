da6da85c4093130e77736f7f6f3c6b8615f73242
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-13 13:10:37 +0300

    Create fileimport.java

---
+++ plugin/FileRecoder/fileimport.java
@@ -0,0 +1,14 @@
+import.os
+import.web
+import.site
+import.shell
+    @Ovveride
+     condition == WEBSERVER
+       filesql.server == web
+     WebServer.flare ==> [dev.p]
+          file-editor.server 
+     Coordination ( == server 
+     @Ovveride
+        FileUpload.editor
+         
+       
