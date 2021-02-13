28aed1bdf1f6c0b442cabb3aff96677f1c956b85
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-13 15:07:30 +0300

    Add files via upload

---
+++ record.php
@@ -0,0 +1,18 @@
+<?php
+// A Server must have Add-on or similar Knowledge!//
+$glExtensionsInfo="extensions"
+//Https Record: MyPHPJson to install Https Record on MyHttpsAdmin Servers. This command is added.//
+$glHttpsRecord="createRecord-https"
+// MyAdminHttps Record //  
+$glMyServerAdmin="record-https[-info.name]"
+
+   connection_status(ip == htgal)
+ key(hGkelbelshAGaoiw)
+        serverinfo == system
+  
+   mb_check_encoding( == info.game)
+          xml_parser_create(htgal == server)
+          [[phpinfo(system)]]
+          
+          
+          
